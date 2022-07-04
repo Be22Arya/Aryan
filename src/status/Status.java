9da553f81250f9b7f3aab3d557fb7831f9e4abe5
Khullabh <107092094+Khullabh@users.noreply.github.com>
2022-07-04 20:36:47 +0530

    update Staus.java for Midterm exam
    update Staus.java for Midterm exam

--- src/status/Status.java
+++ src/status/Status.java
@@ -7,6 +7,8 @@ package status;
 
 import java.util.Scanner;
 
+import status.StausUser.StatusCode;
+
 /** This class takes String input from user 
  * and calls method statusDetail to print the 
  * status of User-Rejected,Pending,Processing and Approved.
@@ -23,11 +25,17 @@ public class Status {
      */
     public static void main(String[] args)
     {
-    Scanner in =new Scanner(System.in);
-    System.out.println("Enter the user status code (zero,one,two,three) in string");
-    String code = in.next();
-    StausUser t= new StausUser();
-    t.statusDetail(code); 
+	    Scanner in =new Scanner(System.in);
+	    System.out.println("Enter the user status code (zero,one,two,three) in string");
+	    String code = in.next();
+	    StausUser t= new StausUser();
+	    try {
+	    	t.setCurrentView(StatusCode.valueOf(code.toUpperCase()));
+	    }
+	    catch (Exception e) {
+	    	t.setCurrentView(StatusCode.NONE);
+	    }
+	    t.statusDetail(code); 
     }
     
 }
